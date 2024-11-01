2b143962d24fed18e31765f38cbe1cd03f0b2d0f
mac-admin <karpushin@gkres.ru>
2024-11-01 17:55:17 +1100

    1.20 Layouts for Work app + Route in Demo app

---
+++ demo/src/demo0/DemoCounter.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <button>-</button>
+    <span>Current value: 0</span>
+    <button>+</button>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ frontend/src/layouts/AppLayout.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+import AppLayoutHeader from "@/layouts/AppLayoutHeader.vue";
+</script>
+
+<template>
+  <div class="app_layout">
+    <app-layout-header />
+    <slot/>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
