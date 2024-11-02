1ebfe2af1b49c2cee55ea8f6964a84fad8541e1b
mac-admin <karpushin@gkres.ru>
2024-11-02 15:42:29 +1100

    1.38 Добавление стилей элементам

--- frontend/src/layouts/AppLayout.vue
+++ frontend/src/layouts/AppLayout.vue
@@ -10,5 +10,9 @@ import AppLayoutHeader from "@/layouts/AppLayoutHeader.vue";
 </template>
 
 <style scoped lang="scss">
-
+.app_layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
 </style>
